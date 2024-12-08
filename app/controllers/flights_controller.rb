ff95165e34b5969c1ead16f5136fd138a3ca7504
biniyamNegasa <104790161+biniyamNegasa@users.noreply.github.com>
2024-12-08 16:31:33 +0300

    feat: pick a flight

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,5 +4,18 @@ class FlightsController < ApplicationController
     @dates = Flight.select(:start_datetime).map do |flight|
                 flight.start_datetime.strftime("%d %B %Y")
              end.uniq
+    current_data = flight_params
+    if current_data && current_data[:date]
+      @flights = Flight.where("departure_airport_id = ? AND arrival_airport_id = ? AND start_datetime BETWEEN ? AND ?",
+                              Airport.where(airport_code: current_data[:departure_airport]).first,
+                              Airport.where(airport_code: current_data[:arrival_airport]).first,
+                              Date.strptime(current_data[:date], "%d %B %Y"),
+                              DateTime.strptime(current_data[:date], "%d %B %Y") + 23.hours + 59.minutes)
+    end
+  end
+
+  private
+  def flight_params
+    params.permit(:departure_airport, :arrival_airport, :number_of_passengers, :date)
   end
 end
